181891b25c332673f8cbdc7346ebe351dfc62edb
CHANDRA HARSHITHA <harshitha17102003@gmail.com>
2025-04-02 20:25:09 +0530

    Delete Day_10 directory

--- Day_10/csv_batch_importer/src/main/java/com/csvimporter/database/Main.java
+++
@@ -1,9 +0,0 @@
-package com.csvimporter.database;
-
-import com.csvimporter.utils.CSVImporter;
-
-public class Main {
-    public static void main(String[] args) {
-        CSVImporter.importCSV();
-    }
-}
